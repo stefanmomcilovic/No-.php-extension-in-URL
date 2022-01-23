00c1e77e74cedd2cb715329f47fbd7cb0f0307cc
Stefan Momcilovic <stefanmomcilovic@Stefans-MacBook-Pro.local>
2022-01-24 00:38:25 +0100

    no .php in url

---
+++ admin/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Admin home page</title>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<ul>
+        <li><a href="index">Home</a></li>
+        <li><a href="blog?id=244">Blog</a></li>
+        <li><a href="about">About</a></li>
+    </ul>
+
+    <h1>Admin home page</h1>
+</body>
+</html>
---
+++ blog.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Blog page</title>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <ul>
+        <li><a href="index">Home</a></li>
+        <li><a href="blog?id=244">Blog</a></li>
+        <li><a href="about">About</a></li>
+    </ul>
+
+    <h1>Blog page</h1>
+</body>
+</html>
